2bbc7d0d33d58a3b680b82d6f05fe9de48bae34f
wx <wangxiangc@gmail.com>
2015-07-24 23:42:28 +0800

    software-doc

---
+++ 操作系统维护/Windows维护/BAT/copyso.bat
@@ -0,0 +1,17 @@
+@echo off
+rem ddk
+REM dd
+cd %~dp0
+:BUILD
+call ndk-build V=1 NDK_DEBUG=0
+:WAIT
+set /p KEY=
+cls
+if "%KEY%"=="C" (
+  call ndk-build clean
+  set KEY=
+  goto WAIT
+
+) else (
+  goto BUILD
+)
